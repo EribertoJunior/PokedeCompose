01cabab4338cad615d36587116d1f5745980cf2e
Eriberto Junior <eriberto.junior@ifood.com.br>
2024-06-04 17:43:41 -0300

    criado ConverterSpecieToEvolutionTest.kt

---
+++ app/src/test/java/com/example/pokedexcompose/data/dataBase/local/converters/ConverterSpecieToEvolutionTest.kt
@@ -0,0 +1,40 @@
+package com.example.pokedexcompose.data.dataBase.local.converters
+
+import com.example.pokedexcompose.data.dataBase.local.entities.SpecieToEvolution
+import io.mockk.spyk
+import org.junit.Assert.*
+import org.junit.Test
+
+class ConverterSpecieToEvolutionTest {
+
+    private var converterSpecieToEvolution = spyk(ConverterSpecieToEvolution())
+
+    @Test
+    fun `should return a jsonArray when a SpecieToEvolution list is passed`() {
+
+        val jsonArray = "[{\"name\":\"name\",\"imageUrl\":\"/321\"}]"
+        val listOf = listOf(
+            SpecieToEvolution(name = "name", imageUrl = "/321")
+        )
+
+        run {
+            val listString = converterSpecieToEvolution.saveListToListString(listOf)
+            assertEquals(jsonArray, listString)
+        }
+    }
+
+    @Test
+    fun `should return a list of SpecieToEvolution when a jsonArray is passed`() {
+
+        val jsonArray = "[{\"name\":\"name\",\"imageUrl\":\"/321\"}]"
+
+        val listOf = listOf(
+            SpecieToEvolution(name = "name", imageUrl = "/321")
+        )
+
+        run {
+            val list = converterSpecieToEvolution.restoreList(jsonArray)
+            assertEquals(listOf, list)
+        }
+    }
+}
