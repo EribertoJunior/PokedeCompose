de03e8f5673ed47b4101f2da547795c4c65bc3e0
Eriberto Junior <eriberto.junior@ifood.com.br>
2024-06-04 17:44:31 -0300

    criado ConverterTypeColoursEnumTest.kt

---
+++ app/src/test/java/com/example/pokedexcompose/data/dataBase/local/converters/ConverterTypeColoursEnumTest.kt
@@ -0,0 +1,42 @@
+package com.example.pokedexcompose.data.dataBase.local.converters
+
+import com.example.pokedexcompose.data.model.local.enums.TypeColoursEnum
+import io.mockk.spyk
+import org.junit.Assert.*
+import org.junit.Test
+
+class ConverterTypeColoursEnumTest {
+
+    private var converterTypeColoursEnum = spyk(ConverterTypeColoursEnum())
+
+    @Test
+    fun `should return a jsonArray when a list of TypeColoursEnum is passed`() {
+
+        val jsonArray = "[\"DRAGON\",\"FIRE\"]"
+        val listOf = listOf(
+            TypeColoursEnum.DRAGON,
+            TypeColoursEnum.FIRE
+        )
+
+        run {
+            val listString = converterTypeColoursEnum.saveListEnumToListString(listOf)
+            assertEquals(jsonArray, listString)
+        }
+    }
+
+    @Test
+    fun `should return a list of TypeColoursEnum when a jsonArray is passed`() {
+
+        val jsonArray = "[\"DRAGON\",\"FIRE\"]"
+
+        val listOf = listOf(
+            TypeColoursEnum.DRAGON,
+            TypeColoursEnum.FIRE,
+        )
+
+        run {
+            val list = converterTypeColoursEnum.restoreListEnum(jsonArray)
+            assertEquals(listOf, list)
+        }
+    }
+}
