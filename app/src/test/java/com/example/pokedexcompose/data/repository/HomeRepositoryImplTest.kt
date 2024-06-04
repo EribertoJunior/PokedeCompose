b53da0236392a6d1afcd7f0b30edaaf092b61e53
Eriberto Junior <eriberto.junior@ifood.com.br>
2024-06-04 18:17:38 -0300

    criado HomeRepositoryImplTest.kt

---
+++ app/src/test/java/com/example/pokedexcompose/data/repository/HomeRepositoryImplTest.kt
@@ -0,0 +1,13 @@
+package com.example.pokedexcompose.data.repository
+
+import org.junit.Assert.*
+
+class HomeRepositoryImplTest {
+
+    /**
+    HomeRepositoryImpl contem um unico metodo (getPokemonList(): Flow<PagingData<PokemonAndDetail>>),
+    porém, PagingData é apenas um fluxo sem estado de eventos de carga incremental, portanto, não dá
+    para analizar o estado do conteudo.
+     */
+
+}
