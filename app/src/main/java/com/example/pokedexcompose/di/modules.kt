668f8fe446414444ad7065416e22a6b4597c27ed
Eriberto Junior <eriberto.junior@ifood.com.br>
2024-06-04 15:52:55 -0300

    Configuração de injeção de dependência

---
+++ app/src/main/java/com/example/pokedexcompose/di/modules.kt
@@ -0,0 +1,70 @@
+package com.example.pokedexcompose.di
+
+import com.example.pokedexcompose.data.dataBase.local.RoomConfig
+import com.example.pokedexcompose.data.dataBase.remote.PokemonPagingSource
+import com.example.pokedexcompose.data.dataBase.remote.RetrofitConfig
+import com.example.pokedexcompose.data.dataSource.local.LocalDataSource
+import com.example.pokedexcompose.data.dataSource.local.LocalDataSourceImpl
+import com.example.pokedexcompose.data.dataSource.remote.RemoteDataSource
+import com.example.pokedexcompose.data.dataSource.remote.RemoteDataSourceImpl
+import com.example.pokedexcompose.data.repository.DetailRepository
+import com.example.pokedexcompose.data.repository.DetailRepositoryImpl
+import com.example.pokedexcompose.data.repository.HomeRepository
+import com.example.pokedexcompose.data.repository.HomeRepositoryImpl
+import com.example.pokedexcompose.data.repository.PokemonRemoteMediator
+import com.example.pokedexcompose.ui.viewmodels.DetailsViewModel
+import com.example.pokedexcompose.ui.viewmodels.HomeViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+const val PROPERTY_BASE_URL = "SERVER_URL"
+
+val modules = module {
+    single {
+        val baseUrl = getProperty<String>(PROPERTY_BASE_URL)
+        RetrofitConfig(baseUrl, androidContext()).getPokeServide()
+    }
+    single { RoomConfig.getDataBase(androidContext()) }
+
+    factory { PokemonPagingSource(get()) }
+
+    factory<HomeRepository> {
+        HomeRepositoryImpl(
+            pokemonRemoteMediator = get(),
+            pokemonDao = get()
+        )
+    }
+    factory<DetailRepository> {
+        DetailRepositoryImpl(
+            localDataSource = get()
+        )
+    }
+
+    factory { get<RoomConfig>().pokemonDao() }
+    factory { get<RoomConfig>().pokemonRemoteKeyDao() }
+    factory { get<RoomConfig>().pokemonDetailDao() }
+    factory { get<RoomConfig>().pokemonSpeciesDao() }
+    factory { get<RoomConfig>().pokemonEvolutionChainDao() }
+
+    factory<RemoteDataSource> { RemoteDataSourceImpl(get()) }
+    factory<LocalDataSource> {
+        LocalDataSourceImpl(
+            pokemonDao = get(),
+            pokemonDetailDao = get(),
+            pokemonSpeciesDao = get(),
+            evolutionChainDao = get(),
+            pokemonRemoteKeyDao = get()
+        )
+    }
+
+    factory {
+        PokemonRemoteMediator(
+            remoteDataSource = get(),
+            localDataSource = get()
+        )
+    }
+
+    viewModel { HomeViewModel(repository = get()) }
+    viewModel { DetailsViewModel(detailRepository = get()) }
+}
