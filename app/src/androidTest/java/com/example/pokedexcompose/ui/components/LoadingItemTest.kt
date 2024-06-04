de1da3060964b2eff4b157f46879f97981fca57c
Eriberto Junior <eriberto.junior@ifood.com.br>
2024-06-04 17:29:01 -0300

    Criado testes para LoadingItemTest.kt

---
+++ app/src/androidTest/java/com/example/pokedexcompose/ui/components/LoadingItemTest.kt
@@ -0,0 +1,22 @@
+package com.example.pokedexcompose.ui.components
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import org.junit.Rule
+import org.junit.Test
+
+class LoadingItemTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun loadingItemTest() {
+        composeTestRule.setContent {
+            LoadingItem()
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription("Loading Pokemons")
+    }
+}
